5e2e17ee2f2984105f7d17fba2ddb9a8befdf89b
Nikita <nikita-vorotyncev@yandex.ru>
2025-12-09 14:49:31 +0000

    Deleted doctype tasks,task,project

--- tasks_app/tasks_app/doctype/project/project.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, n and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class project(Document):
-	pass
